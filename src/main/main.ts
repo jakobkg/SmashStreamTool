cde0428364e6265ef0a8af8f46b95440101b2dcf
Jakob Grønhaug <jakobkg@gmail.com>
2020-11-22 23:29:40 +0100

    More helper functions

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,6 +1,5 @@
 import { app, BrowserWindow, IpcMain } from 'electron';
 import { SlpLiveStream, SlpRealTime, getCharacterName, getCharacterColorName } from '@vinceau/slp-realtime';
-import { hasTeam, separateTeamAndName } from '@utils/names';
 import * as OBSWebSocket from 'obs-websocket-js';
 import * as path from 'path';
 import * as url from 'url';
@@ -23,6 +22,32 @@ realtime.setStream(livestream);
 // Initialize OBS websocket instance
 const obs = new OBSWebSocket();
 
+
+// Connect to the Slippi relay
+livestream.start(SLIPPIADDRESS, SLIPPIPORT)
+  .then(() => {
+    if (process.env.NODE_ENV != 'production'){
+      console.log('Connecting to Slippi Relay');
+    }
+  }).catch(() => {
+    return
+  });
+
+// Connect to the OBS websocket
+obs.connect({ address: OBSADDRESS + ':' + OBSPORT })
+  .catch(() => {
+    if (process.env.NODE_ENV != 'production'){
+      console.log('Could not connect to OBS websocket');
+    }
+});
+
+obs.on('ConnectionOpened', () => {
+  if (process.env.NODE_ENV != 'production'){
+    console.log('Connected to OBS websocket');
+  }
+});
+
+// Set Electron window settings
 function createWindow(): void {
   mainWindow = new BrowserWindow({
     width: 800,
@@ -48,6 +73,7 @@ function createWindow(): void {
   });
 }
 
+// Electron listeners
 app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
@@ -62,21 +88,11 @@ app.on('activate', () => {
   }
 });
 
-
-// Connect to the Slippi relay
-livestream.start(SLIPPIADDRESS, SLIPPIPORT)
-  .then(() => {
-    console.log('Connecting to Slippi Relay');
-  }).catch(() => {
-    return
-  });
-
-// Connect to the OBS websocket
-obs.connect({ address: OBSADDRESS + ':' + OBSPORT })
-  .catch(() => {
-    console.log('Could not connect to OBS Websocket :(');
+// Slippi listeners
+realtime.game.start$.subscribe( (slippiData) => {
+  //TODO: Hent karakterer, skins og ports og send til overlay
 });
 
-obs.on('ConnectionOpened', () => {
-  console.log('Connected to OBS websocket :)');
+realtime.game.end$.subscribe( (slippiData) => {
+  //TODO: Se hvem som vant, oppdater score
 });
--- src/utils/obs.ts
+++ src/utils/obs.ts
@@ -1,4 +1,5 @@
 import * as OBSWebSocket from 'obs-websocket-js';
+import { stringify, parse } from 'querystring';
 
 export function updateTextSource(obs: OBSWebSocket, sourceName: string, contents: string): void {
   obs.send('SetSourceSettings', {sourceName: sourceName, sourceSettings: {'text': contents}})
@@ -10,7 +11,7 @@ export function updateTextSource(obs: OBSWebSocket, sourceName: string, contents
   .catch(() => {
     if (process.env.NODE_ENV != 'production'){
       console.log('Failed to update text source ' + sourceName);
-  }
+    }
   });
 }
 
@@ -26,4 +27,32 @@ export function updateWebSource(obs: OBSWebSocket, sourceName: string, sourceSet
       console.log('Failed to update web source ' + sourceName);
     }
   });
+}
+
+export function updateBracketView(obs: OBSWebSocket, bracketSourceName: string, bracketLink?: string, settings?: {multiplier?: number, match_width_multiplier?: number, show_tournament_name?: number}): void {
+  let assembledUrl: string;
+  let baseUrl: string;
+  let querystring: string;
+
+  if (!bracketLink && !settings) {
+    if (process.env.NODE_ENV != 'production'){
+      console.log('Invalid call to updateBracketView(), no link or settings provided');
+    }
+    return;
+  }
+
+  if (!bracketLink) {
+    obs.sendCallback('GetSourceSettings', { sourceName: bracketSourceName }, (err, data)=> {
+      //TODO Get base url from current settings
+    });
+  } else if (!settings) {
+    obs.sendCallback('GetSourceSettings', { sourceName: bracketSourceName }, (err, data)=> {
+      //TODO Get querystring from current settings
+    });
+  } else {
+    assembledUrl = bracketLink + '/module?' + stringify(settings);
+  }
+
+
+  updateWebSource(obs, bracketSourceName, {url: assembledUrl}); //TODO fix error
 }
