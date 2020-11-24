4c23b776fd153ea356a1729fc36f10c4faa39839
Jakob Grønhaug <jakobkg@gmail.com>
2020-11-24 17:34:28 +0100

    Remove unneeded code

--- src/utils/obs.ts
+++ src/utils/obs.ts
@@ -1,5 +1,5 @@
 import * as OBSWebSocket from 'obs-websocket-js';
-import { stringify, parse } from 'querystring';
+import  * as qs from 'querystring';
 
 export function updateTextSource(obs: OBSWebSocket, sourceName: string, contents: string): void {
   obs.send('SetSourceSettings', {sourceName: sourceName, sourceSettings: {'text': contents}})
@@ -27,32 +27,4 @@ export function updateWebSource(obs: OBSWebSocket, sourceName: string, sourceSet
       console.log('Failed to update web source ' + sourceName);
     }
   });
-}
-
-export function updateBracketView(obs: OBSWebSocket, bracketSourceName: string, bracketLink?: string, settings?: {multiplier?: number, match_width_multiplier?: number, show_tournament_name?: number}): void {
-  let assembledUrl: string;
-  let baseUrl: string;
-  let querystring: string;
-
-  if (!bracketLink && !settings) {
-    if (process.env.NODE_ENV != 'production'){
-      console.log('Invalid call to updateBracketView(), no link or settings provided');
-    }
-    return;
-  }
-
-  if (!bracketLink) {
-    obs.sendCallback('GetSourceSettings', { sourceName: bracketSourceName }, (err, data)=> {
-      //TODO Get base url from current settings
-      updateWebSource(obs, bracketSourceName, {url: assembledUrl});
-    });
-  } else if (!settings) {
-    obs.sendCallback('GetSourceSettings', { sourceName: bracketSourceName }, (err, data)=> {
-      //TODO Get querystring from current settings
-      updateWebSource(obs, bracketSourceName, {url: assembledUrl});
-    });
-  } else {
-    assembledUrl = bracketLink + '/module?' + stringify(settings);
-    updateWebSource(obs, bracketSourceName, {url: assembledUrl});
-  }
 }
