6bea79acb2774d38196c03e7b4f97dc9e0828a7f
Jakob Grønhaug <jakobkg@gmail.com>
2020-12-01 10:54:44 +0100

    More interfaces

---
+++ src/common/types/GuiData.ts
@@ -0,0 +1,11 @@
+export interface GuiDataInterface {
+  p1FullName: string;
+  p1Score: number;
+
+  p2FullName: string;
+  p2Score: number;
+
+  round: string;
+  tournamentName: string;
+  bestOf: number;
+}
---
+++ src/common/types/OverlayData.ts
@@ -0,0 +1,34 @@
+export interface OverlayData {
+  p1Name: string;
+  p1Team: string;
+
+  p1Character: string;
+  p1Skin: string;
+
+  p1Color: number;
+  p1Score: number;
+  p1WL: string;
+
+  p2Name: string;
+  p2Team: string;
+
+  p2Character: string;
+  p2Skin: string;
+
+  p2Color: number;
+  p2Score: number;
+  p2WL: string;
+
+  bestOf: string;
+  round: string;
+  tournamentName: string;
+
+  caster1Name: string;
+  caster1Twitter: string;
+  caster1Twitch: string;
+
+  caster2Name: string;
+  caster2Twitter: string;
+  caster2Twitch: string;
+  allowIntro: boolean;
+}
